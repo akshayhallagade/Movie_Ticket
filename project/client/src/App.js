dd3fc15edb842e33afabe3888172cfa78dcc2ed6
Akshay Hallagade <akshayhallagade2612@gmail.com>
2024-05-23 14:43:22 +0530

    init

---
+++ project/client/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
